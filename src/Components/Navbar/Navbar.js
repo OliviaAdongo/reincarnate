fe791c1e1f4d6356a7cc28af5764b65c6e5b2bea
Olivia Adongo <akinyioliviatove@gmail.com>
2023-04-10 14:03:16 +0300

    Milestone: Navbar added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -13,15 +13,15 @@ const showNavbar =()=> {
   return ( 
     <div>
       <header className="header">
-        <img
+        {/* <img
           className="navbar-logo"
           src="http://testdeploy.thesaruni.com/SaruniLogo.png"
           alt="logo"
           onClick={() => {
             navigate("/");
           }}
-        />
-        <h4 className="the">THE SARUNI</h4>
+        /> */}
+        {/* <h4 className="the">THE SARUNI</h4> */}
         <nav ref={navRef}>
           <a href="/menu" className="bars"><FaBars/></a>
           
