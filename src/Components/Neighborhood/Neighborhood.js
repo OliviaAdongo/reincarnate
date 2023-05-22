41c0b0f83cd65107be34071cc76db07af19edf31
Olivia Adongo <akinyioliviatove@gmail.com>
2023-05-22 14:08:17 +0300

    Milestone: Neighborhood edits

--- src/Components/Neighborhood/Neighborhood.js
+++ src/Components/Neighborhood/Neighborhood.js
@@ -6,7 +6,6 @@ function Neighborhood() {
   let navigate = useNavigate();
   return (
     <div className="parent-div-neighborhood">
-     
       <div className="flexbox-container-neighborhood-home">
         <div className="flexbox-item-neighborhood-home-1">
           <div className="heading">
@@ -16,7 +15,7 @@ function Neighborhood() {
               className="logo"
               alt="logo"
             />
-          </div> 
+          </div>
 
           <div className="page-title">
             <h1 className="page-topic"> THE NEIGHBORHOOD </h1>
@@ -25,9 +24,12 @@ function Neighborhood() {
           <div className="left-body-content">
             <p className="first-paragraph">
               Coddled by nature on the edge of Nairobi River, 'The Saruni', is a
-              tranquil refuge located on Nairobi's coveted Riverside Drive; A
-              secluded preference for embassies, diplomatic organizations and
-              professional companies.
+              tranquil refuge located on Nairobi's coveted Riverside Drive;
+          
+            </p>
+            <p className="second-paragraph">
+            A secluded preference for embassies, diplomatic
+              organizations and professional companies.
             </p>
             <p className="second-paragraph">
               Riverside Drive is a premium expression of privacy, prestige and
@@ -45,8 +47,10 @@ function Neighborhood() {
               <li className="place">Nairobi CBD</li>
             </ul>
             <h5 className="bottom-heading">BYPASS CONECTIVITY</h5>
-            <p className="jkia">20 min from JKIA via the Expressway <br/> Easy access to Ngong' Road, Kileleshwa, Kilimani and Loresho.</p>
-           
+            <p className="jkia">
+              20 min from JKIA via the Expressway <br /> Easy access to Ngong'
+              Road, Kileleshwa, Kilimani and Loresho.
+            </p>
           </div>
         </div>
         <div className="flexbox-item-neighborhood-home-2">
@@ -56,15 +60,15 @@ function Neighborhood() {
             className="childimage homeneigh"
             loading="lazy"
           />
-           <div className="right-arrow-responsive">
-          <FaAngleRight
-          className="fa fa-arrow-right"
-            onClick={() => {
-              navigate("/map");
-            }}
-            id="arrow-right"
-          />
-        </div>
+          <div className="right-arrow-responsive">
+            <FaAngleRight
+              className="fa fa-arrow-right"
+              onClick={() => {
+                navigate("/map");
+              }}
+              id="arrow-right"
+            />
+          </div>
         </div>
       </div>
     </div>
