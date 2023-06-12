b175e0eda532f51c4694bfe71e6ce11c29ce8c7e
Olivia Adongo <akinyioliviatove@gmail.com>
2023-06-12 13:32:27 +0300

    Milestone: Availability page adjustment

--- src/Components/Availability/Availability.js
+++ src/Components/Availability/Availability.js
@@ -2,150 +2,116 @@ import "./Availability.css";
 
 function Availability() {
   return (
-    <div class="flexbox-container-availability">
-        <div className="flexbox-item-availability1">
-          <div className="lux-heading">
-            <h1 className="topic-lux">LUXURY UNITS</h1>
-            
-          </div> 
-          {/* BODY SECTION */}
-          <div className="sect-body">
+    <div className="flexbox-container">
+    <div className="flexbox-item-availability1">
+      <div className="heading" id='head'>
+        <h1 className="page-topic">LUXURY UNITS</h1>
+      </div>
+      <div className="container">
+        <div className="one">
+          <h2 className="apartment">
+            <p className="number">1</p>
+            BEDROOM APARTMENTS
+          </h2>
 
-            <div className="body-one">
-
-              <div className="lux-body-upjuu">
-               
-                <h2 className="body-lux tittle">
-                  {" "}
-                  <p className="number" style={{ display: "inline" }}>
-                    {" "}
-                    1
-                  </p>{" "}
-                  BEDROOM APARTMENTS
-                </h2>
-
-                <ul>
-                  <li className="body-lux one">
-                    Starting from 77sqm (829 sqft)
-                  </li>
-                  <li className="body-lux one">
-                    Beautiful West side facing Courtyard
-                  </li>
-                  <li className="body-lux one">
-                    {" "}
-                    Open plan Kitchen fit with European appliances
-                  </li>
-                </ul>
-              </div> 
-
-              <div className="lux-body-upple">
-                <h2 className="body-lux tittle">
-                  <p className="number" style={{ display: "inline" }}>
-                    {" "}
-                    2
-                  </p>{" "}
-                  BEDROOM APARTMENTS
-                </h2>
-                <ul>
-                  <li className="body-lux one">
-                    Starting from 132sqm (1421sqft)
-                  </li>
-                  <li className="body-lux one">DSQ with private access</li>
-                  <li className="body-lux one">
-                    {" "}
-                    Open plan Kitchen fit with European appliances
-                  </li>
-                  <li className="body-lux one">
-                    Well-ventilated Laundry Yard
-                  </li>
-                  <li className="body-lux one">
-                    Beautiful useable sizeable balconies
-                  </li>
-                  <li className="body-lux one">
-                    Amazing views both Surbub on North and Either East Side City
-                    View or West Side Views
-                  </li>
-                </ul>
-              </div>
-              
-            </div>
-
-            <div className="body-two">
-              <div className="lux-body-updown">
-                <h2 className="body-lux tittle">
-                  <p className="number" style={{ display: "inline" }}>
-                    {" "}
-                    3
-                  </p>{" "}
-                  BEDROOM APARTMENTS
-                </h2>
-                <ul>
-                  <li className="body-lux one">
-                    {" "}
-                    Starting from 218 sqm (2347sqft)
-                  </li>
-                  <li className="body-lux one">DSQ with private access</li>
-                  <li className="body-lux one">
-                    {" "}
-                    Open plan Kitchen fit with European appliances
-                  </li>
-                  <li className="body-lux one">
-                    Well-ventilated Laundry Yard 
-                  </li>
-                  <li className="body-lux one">
-                    Beautiful useable sizeable balconies
-                  </li>
-                  <li className="body-lux one">
-                    3 beedrooms have spectacular views both Surbub on North and
-                    Either East Side City View or West Side Views
-                  </li>
-                </ul>
-              </div>
-
-              <div className="lux-body">
-                <h2 className="body-lux tittle">
-                  <p className="number" style={{ display: "inline" }}>
-                    {" "}
-                    4
-                  </p>{" "}
-                  DUPLEX 
-                  <p>PENTHOUSES</p>
-                </h2>
-                <ul>
-                  <li className="body-lux one">
-                    {" "}
-                    Starting from 623sqm (6706sqft)
-                  </li>
-                  <li className="body-lux one">Private Rooftop terrace</li>
-                  <li className="body-lux one">
-                    DSQ with private access and well-ventilated
-                  </li>
-                  <li className="body-lux one">
-                    {" "}
-                    Open plan Kitchen fit with European appliances
-                  </li>
-                  <li className="body-lux one">
-                    Luxurious West side facing courtyard views East Side City
-                    View or West Side Views
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-
-          </div>
-         
-
+          <ul>
+            <li className="room-description">
+              Starting from 77sqm (829 sqft)
+            </li>
+            <li className="room-description">
+              Beautiful West side facing Courtyard
+            </li>
+            <li className="room-description">
+              Open plan Kitchen fit with European appliances
+            </li>
+          </ul>
         </div>
-
-        <div class="flexbox-item-availability2">
-          <img
-            src="http://testdeploy.thesaruni.com/avilability.jpg"
-            alt="residence"
-            className="apartment-view"
-          />
+        <div className="two">
+          <h2 className="apartment">
+            <p className="number">2</p>
+            BEDROOM APARTMENTS
+          </h2>
+          <ul>
+            <li className="room-description">
+              Starting from 132sqm (1421sqft)
+            </li>
+            <li className="room-description">DSQ with private access</li>
+            <li className="room-description">
+              Open plan Kitchen fit with European appliances
+            </li>
+            <li className="room-description">
+              Well-ventilated laundry yard
+            </li>
+            <li className="room-description">
+              Beautiful useable sizeable balconies
+            </li>
+            <li className="room-description">
+              Amazing views both Surbub on North and either East Side City
+              View or West Side Views
+            </li>
+          </ul>
+        </div>
+        <div className="three">
+          <h2 className="apartment">
+            <p className="number">3</p>
+            BEDROOM APARTMENTS
+          </h2>
+          <ul>
+            <li className="room-description">
+              {" "}
+              Starting from 218 sqm (2347sqft)
+            </li>
+            <li className="room-description">DSQ with private access</li>
+            <li className="room-description">
+              {" "}
+              Open plan Kitchen fit with European appliances
+            </li>
+            <li className="room-description">
+              Well-ventilated Laundry Yard
+            </li>
+            <li className="room-description">
+              Beautiful useable sizeable balconies
+            </li>
+            <li className="room-description">
+              3 beedrooms have spectacular views both Surbub on North and
+              Either East Side City View or West Side Views
+            </li>
+          </ul>
+        </div>
+        <div className="four">
+          <h2 className="apartment">
+            <p className="number">4</p>
+            DUPLEX PENTHOUSES
+          </h2>
+          <ul>
+            <li className="room-description">
+              {" "}
+              Starting from 623sqm (6706sqft)
+            </li>
+            <li className="room-description">Private Rooftop terrace</li>
+            <li className="room-description">
+              DSQ with private access and well-ventilated
+            </li>
+            <li className="room-description">
+              {" "}
+              Open plan Kitchen fit with European appliances
+            </li>
+            <li className="room-description">
+              Luxurious West side facing courtyard views East Side City View
+              or West Side Views
+            </li>
+          </ul>
         </div>
       </div>
+    </div>
+    <div className="flexbox-item-availability2">
+      <img
+        src="http://testdeploy.thesaruni.com/avilability.jpg"
+        alt="residence"
+        className="side-image"
+      />
+    </div>
+  </div>
   );
 }
 
